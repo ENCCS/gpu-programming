1dea64eb5e33b0d5e34d7486470a4a896eebb9d1
Stepas Toliautas <59330245+stepas-toliautas@users.noreply.github.com>
2023-04-04 11:25:58 +0300

    Add SYCL files for stencil example

---
+++ content/examples/stencil/sycl/core.cpp
@@ -0,0 +1,43 @@
+#include "heat.h"
+#include <sycl/sycl.hpp>
+using namespace sycl;
+
+// Update the temperature values using five-point stencil
+// Arguments:
+//   queue: SYCL queue
+//   curr: current temperature values
+//   prev: temperature values from previous time step
+//   a: diffusivity
+//   dt: time step
+void evolve(field *curr, field *prev, double a, double dt)
+{
+  // Help the compiler avoid being confused by the structs
+  double *currdata = curr->data.data();
+  double *prevdata = prev->data.data();
+  int nx = prev->nx;
+  int ny = prev->ny;
+
+  // Determine the temperature field at next time step
+  // As we have fixed boundary conditions, the outermost gridpoints
+  // are not updated.
+  double dx2 = prev->dx * prev->dx;
+  double dy2 = prev->dy * prev->dy;
+
+  {
+    buffer<double, 2> buf_curr { currdata, range<2>(nx + 2, ny + 2) },
+                      buf_prev { prevdata, range<2>(nx + 2, ny + 2) };
+
+    Q.submit([&](handler &cgh) {
+      auto acc_curr = accessor(buf_curr, cgh, read_write);
+      auto acc_prev = accessor(buf_prev, cgh, read_only);
+
+      cgh.parallel_for(range<2>(nx, ny), [=](id<2> id) {
+        auto j = id[0] + 1;
+        auto i = id[1] + 1;
+        acc_curr[j][i] = acc_prev[j][i] + a * dt *
+            ((acc_prev[j][i + 1] - 2.0 * acc_prev[j][i] + acc_prev[j][i - 1]) / dx2 +
+             (acc_prev[j + 1][i] - 2.0 * acc_prev[j][i] + acc_prev[j - 1][i]) / dy2);
+      });
+    });
+  }
+}
---
+++ content/examples/stencil/sycl/heat.h
@@ -0,0 +1,59 @@
+#ifndef __HEAT_H__
+#define __HEAT_H__
+
+#include <sycl/sycl.hpp>
+#include <vector>
+
+// Datatype for temperature field
+struct field {
+    // nx and ny are the dimensions of the field. The array data
+    // contains also ghost layers, so it will have dimensions nx+2 x ny+2
+    int nx;
+    int ny;
+    // Size of the grid cells
+    double dx;
+    double dy;
+    // The temperature values in the 2D grid
+    std::vector<double> data;
+};
+
+// CONSTANTS
+// Fixed grid spacing
+const double DX = 0.01;
+const double DY = 0.01;
+// Default temperatures
+const double T_DISC = 5.0;
+const double T_AREA = 65.0;
+const double T_UPPER = 85.0;
+const double T_LOWER = 5.0;
+const double T_LEFT = 20.0;
+const double T_RIGHT = 70.0;
+// Default problem size
+const int NSTEPS = 500;
+const int ROWS = 2000;
+const int COLS = 2000;
+
+
+// Function prototypes
+void initialize(int argc, char *argv[], field *heat1,
+                field *heat2, int *nsteps);
+
+void evolve(sycl::queue &Q, field *curr, field *prev, double a, double dt);
+
+void field_set_size(field *heat, int nx, int ny);
+
+void field_generate(field *heat);
+
+double field_average(field *heat);
+
+void field_write(field *heat, int iter);
+
+void field_create(field *heat1, field *heat2, int rows, int cols);
+
+void field_copy(field *heat1, field *heat2);
+
+void field_swap(field *heat1, field *heat2);
+
+void field_allocate(field *heat);
+
+#endif  // __HEAT_H__
