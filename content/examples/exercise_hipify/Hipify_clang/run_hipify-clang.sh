3a5c013193c12311b1d66d63425917811c2783d8
Hicham Agueny <95568317+HichamAgueny@users.noreply.github.com>
2023-06-12 00:02:47 +0200

    add rocm version and delete comments

--- content/examples/exercise_hipify/Hipify_clang/run_hipify-clang.sh
+++ content/examples/exercise_hipify/Hipify_clang/run_hipify-clang.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 MyDir=/scratch/project_465000485
-#MyContainer=${MyDir}/Container/cuda_11.4.0-devel-ubuntu20.04.sif
 MyContainer=${MyDir}/Container/cuda_11.4.3-devel-ubuntu20.04.sif
 
-module load rocm
-
-
-#singularity shell -B $PWD,/opt:/opt /scratch/project_465000485/Container/cuda_11.4.0-devel-ubuntu20.04.sif
+module load rocm/5.2.3
 
 singularity exec -B $PWD,/opt:/opt ${MyContainer} bash -c \
           "export PATH=/opt/rocm-5.2.3/bin:$PATH && \
